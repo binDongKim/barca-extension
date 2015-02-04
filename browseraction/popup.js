3a1fa60af45a0e5a0cbc267478f8dec272766fee
KimDongBin <db@akaiv.com>
2015-02-04 15:24:12 +0900

    Client-side: POST data through [jquery] Server-side: append data to local text file [node,express]

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,7 +1,11 @@
-window.onload = function() {
-	// document.getElementById("button").onclick = function() {
-	// 	chrome.extension.sendMessage({
-	//         type: "color-divs"
-	//     });
-	// }
-}
+(function ($) {
+  $(function () {
+    $('#form-expression').submit(function (ev) {
+      var expression = $('#expression').val();
+      $.post('http://localhost:8000/expressions', { expression: expression }, function (data) {
+        console.log(data);
+      });
+      ev.preventDefault();
+    });
+  });
+})(jQuery);
---
+++ server.js
@@ -0,0 +1,23 @@
+var express    = require('express'),
+    fs         = require('fs'),
+    bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(
+  "/", //the URL throught which you want to access to you static content
+  express.static(__dirname) // where your static content is located in your filesystem
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/expressions', function (req, res) {
+  var expression = req.body.expression;
+  fs.appendFile('/Users/qoo/Documents/English - Nice Expressions.txt', '\n' + expression, function (err) {
+    if (err) return console.log(err);
+    res.end('Success');
+  });
+});
+
+app.listen(8000, function () {
+  console.log('Server runs on 8000');
+});
