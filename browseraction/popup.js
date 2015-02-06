83401a7127b538c5cef38115206591cdd06d1429
KimDongBin <db@akaiv.com>
2015-02-06 13:19:13 +0900

    Case handling: Content is empty

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -2,17 +2,23 @@
   $(function () {
     $('#form-expression').submit(function (ev) {
       var expression = $('#expression').val();
-
-      $.post('http://localhost:8000/expressions', { expression: expression }, function (res) {
-        if (res === 'Success') {
-          swal('Added!');
-        } else {
-          swal('Something wrong!');
-        }
-      }).fail(function () {
-        swal('Run Server first!');
-      });
-      ev.preventDefault();
+      if (! expression)
+      {
+        swal('Content required!');
+      }
+      else
+      {
+        $.post('http://localhost:8000/expressions', { expression: expression }, function (res) {
+          if (res === 'Success') {
+            swal('Added!');
+          } else {
+            swal('Something wrong!');
+          }
+        }).fail(function () {
+          swal('Run Server first!');
+        });
+      }
+      return ev.preventDefault() && false;
     });
 
     $('#clean-button').click(function (ev) {
