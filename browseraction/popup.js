5cc5e9782a5d4b51015460122fd4999bcee328ec
KimDongBin <db@akaiv.com>
2015-02-06 13:03:41 +0900

    Add 'clean' button

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -2,16 +2,21 @@
   $(function () {
     $('#form-expression').submit(function (ev) {
       var expression = $('#expression').val();
+
       $.post('http://localhost:8000/expressions', { expression: expression }, function (res) {
         if (res === 'Success') {
           swal('Added!');
         } else {
           swal('Something wrong!');
         }
-      }).fail(function() {
+      }).fail(function () {
         swal('Run Server first!');
       });
       ev.preventDefault();
+    });
+
+    $('#clean-button').click(function (ev) {
+      $('#expression').val('');
     });
   });
 })(jQuery);
